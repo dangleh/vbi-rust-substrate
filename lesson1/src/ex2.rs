56516784b751eb9c99d2c964bc1a6696f4893ef7
Le Hai Dang <lehaidangdev@gmail.com>
2022-06-11 14:35:59 +0700

    done lesson1/ex2

--- lesson1/src/ex2.rs
+++ lesson1/src/ex2.rs
@@ -1,5 +1,21 @@
 // Cho 1 chuỗi str Slice như dưới đây. Nhập 1 từ bất kỳ từ bàn phím, in ra số lượng từ này xuất hiện trong chuỗi đã cho.
 // Nâng cao hơn : Tìm kiếm không phân biêt chữ hoa thường, theo dạng regex.
-// https://ars.els-cdn.com/content/image/1-s2.0-S0960982203005347-mmc6.txt
+use std::io::{self, BufRead};
+fn count_occurences_character(source: &str, char: &str) -> usize {
+    return source.trim().matches(char.trim()).count();
+}
 
-fn main() {}
+fn main() {
+    let stdin = io::stdin();
+    let mut source = String::new();
+    println!("Enter your string:");
+    stdin.lock().read_line(&mut source).unwrap();
+    let mut char_input = String::new();
+    println!("Enter your character want to find occurencies:");
+    stdin.lock().read_line(&mut char_input).unwrap();
+    print!(
+        "Found: {} ocurrencies in {}",
+        count_occurences_character(&source, &char_input),
+       &source
+    );
+}
